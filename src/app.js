d9223f5012ec6f034348da247870e23327ad0375
Petter Tchirkov <tchirkov.petter@gmail.com>
2021-02-01 19:21:53 +0200

    redesign

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* src/app.js */
-
-// Styles
 import 'styles/_app.scss';
-
+import 'bootstrap';
 
 $(function() {
   console.log('Ready!');
 
   require('scripts/demo');
 })
+
+function sendEmail(){
+  Email.send({
+    Host : "smtp.yourisp.com",
+    Username : "username",
+    Password : "password",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+  }).then(
+  message => alert(message)
+  );
+}
